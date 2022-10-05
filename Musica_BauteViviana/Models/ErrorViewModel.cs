e976c08af3075be7e7d603f07f805f73817bedb3
Vivs <vivscodes@hotmail.com>
2022-10-05 09:55:51 +0100

    Agregar archivos de proyecto.

---
+++ Musica_BauteViviana/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Musica_BauteViviana.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
